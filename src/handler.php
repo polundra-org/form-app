a4475226c43161ccffe8339da6002caee8b74faa
oromadin <osromadin@gmail.com>
2025-02-20 20:28:01 +0200

    add csvRead and csvWrite

--- src/handler.php
+++ src/handler.php
@@ -1,33 +1,45 @@
 <?php
 
+define('CSV_PATH', '../data/names.csv');
+
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $email = $_POST['email'];
 
-$fp = fopen('../data/names.csv', 'a+');
+function csvRead()
+{   
+    $fp = fopen(CSV_PATH, 'r+');
+    $headers = fgetcsv($fp, 100, ',');
 
-// $headers = fgetcsv($fp, 100, ',');
+    while (!feof($fp)) {
+        $line = fgetcsv($fp, 100, ',');
+        var_dump($line);
 
-// $items = [];
+        $item = array_combine($headers, $line); // на послденей иттерации, функиця fgetscvs возвращает false и array_combine дает ошибку, поскольку ждет два массива.
+        echo '<pre>';
+        print_r($item);
+        echo '</pre>';
+    }
 
-// while (!feof($fp)) {
-//     $str = fgetcsv($fp, 1000, ',');
-    
-//     echo '<pre>';
-//     print_r($str);
-//     echo '</pre>';
-    
-   // echo gettype($str);
+    fclose($fp);
+}
 
-    // $item = array_combine($headers, $str);
-    // $items[] = $item;
-// }
+function csvWrite()
+{
+    $fp = fopen(CSV_PATH, 'a+');
+    fputcsv($fp, $_POST, ',');
+    fclose($fp);
+}
+
+function csvEmailExixst()
+{
+
+}
+
+csvWrite();
+//csvRead();
 
 //  fseek($fp, 0, SEEK_END);
-
-fputcsv($fp, $_POST, ',');
-
-fclose($fp);
 
 header("Location: result.php?first_name=$first_name&last_name=$last_name&email=$email");
 
