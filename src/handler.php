fb8ff099dfe15b2a716347c818ecca52f7eaf129
oromadin <osromadin@gmail.com>
2025-03-04 00:53:43 +0200

    use separate file for class

--- src/handler.php
+++ src/handler.php
@@ -1,66 +1,10 @@
 <?php
 
+require_once __DIR__ . '/user.php';
+
 define('DEFAULT_FNAME', 'Andrey');
 define('DEFAULT_LNAME', 'Gubin');
 define('DEFAULT_EMAIL', 'girls_like_stars@gmail.com');
-
-class Handler
-{
-    public function __construct(public string $csvPath) {}
-
-    public function addData(array $request) : bool
-    {
-        if ($this->emailExist($request[2])) {
-            $message = false;
-        } else {
-            $this->wLine($request);
-            $message = true;
-        }
-
-        return $message;
-    }
-
-    private function wLine(array $request) : void
-    {   
-        $fp = fopen($this->csvPath, 'a+');
-        fputcsv($fp, $request, ',');
-        fclose($fp);
-    }
-
-    private function emailExist(string $email) : bool
-    {
-        $fp = fopen($this->csvPath, 'r+');
-        $headers = fgetcsv($fp, 100, ',');
-        
-        while (!feof($fp)) {
-            $item = fgetcsv($fp, 100, ',');
-            
-            if ($item[2] === $email) {
-                return true;
-            }
-        }
-        fclose($fp);
-               
-        return false;
-    }
-
-    private function rLine(string $email) : array | null
-    {
-        $fp = fopen($this->csvPath, 'r+');
-        $headers = fgetcsv($fp, 100, ',');
-        
-        while (!feof($fp)) {
-            $item = fgetcsv($fp, 100, ',');
-            
-            if ($item[2] === $email) {
-                return $item;
-            }
-        }
-        fclose($fp);
-
-        return null;
-    }
-}
 
 if (empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['email'])) {
     $first_name = DEFAULT_FNAME;
@@ -72,9 +16,7 @@ if (empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['e
     $email = $_POST['email'];
 } 
 
-$request = [$first_name, $last_name, $email];
+$user = new User('../data/requests.csv');
+$message = $user->addUser($first_name, $last_name, $email);
 
-$handler = new Handler('../data/requests.csv');
-$message = $handler->addData($request);
-
-header("Location: result.php?first_name=$first_name&last_name=$last_name&email=$email&message=$message");
+header("Location: result.php?email=$email&message=$message");
--- src/result.php
+++ src/result.php
@@ -1,9 +1,17 @@
 <?php
 
-$fullName = $_GET['first_name'] . ' ' . $_GET['last_name'];
+require_once __DIR__ . '/user.php';
+
+$email = $_GET['email'];
+$message = $_GET['message'];
+
+$data = new User('../data/requests.csv');
+$user = $data->readUser($email);
+
+$fullName = $user[0] . ' ' . $user[1];
 
 if ($_GET['message']) {
-    echo "$fullName, Ваши данные сохранены. Запрос на подтверждение отправлен на почту: $_GET[email]";
+    echo "$fullName, Ваши данные сохранены. Запрос на подтверждение отправлен на почту: $email";
 } else {
-    echo "$fullName, Вы уже зарегестрированы. Ваша почта: $_GET[email]";
+    echo "$fullName, Вы уже зарегестрированы. Ваша почта: $email";
 }
