3932b5bd1e09d949c40549bbb8ce6ad62fa6c80a
oromadin <osromadin@gmail.com>
2025-03-03 22:01:03 +0200

    -

--- src/handler.php
+++ src/handler.php
@@ -5,7 +5,6 @@ define('DEFAULT_FNAME', 'Andrey');
 define('DEFAULT_LNAME', 'Gubin');
 define('DEFAULT_EMAIL', 'girls_like_stars@gmail.com');
 
-
 if (empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['email'])) {
     $first_name = DEFAULT_FNAME;
     $last_name = DEFAULT_LNAME;
