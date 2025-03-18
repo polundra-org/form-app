88377abddf3d4f3446865d0854cd38d8bcdbf637
oromadin <osromadin@gmail.com>
2025-03-18 21:46:35 +0200

    add new params to methods

--- src/user.php
+++ src/user.php
@@ -2,23 +2,23 @@
 
 class User
 {
-    public function __construct(public string $csvPath) {}
+    public function __construct(private string $csvPath) {}
 
-    public function addUser(string $first_name, string $last_name, string $email) : bool
+    public function addUser(string $first_name, string $last_name, string $email, int $resends_number, string $last_send) : bool
     {
         if ($this->isUserExist($email)) {
             $message = false;
         } else {
-            $this->writeUser($first_name, $last_name, $email);
+            $this->writeUser($first_name, $last_name, $email, $resends_number, $last_send);
             $message = true;
         }
 
         return $message;
     }
 
-    private function writeUser(string $first_name, string $last_name, string $email) : void
+    private function writeUser(string $first_name, string $last_name, string $email, int $resends_number, string $last_send) : void
     {   
-        $request = [$first_name, $last_name, $email];
+        $request = [$first_name, $last_name, $email, $resends_number, $last_send];
         $fp = fopen($this->csvPath, 'a+');
         fputcsv($fp, $request, ',');
         fclose($fp);
