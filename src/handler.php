230c6dbf23c0113efba8c5b5049920ce8eafd133
oromadin <osromadin@gmail.com>
2025-03-18 21:44:34 +0200

    add initial values

--- src/handler.php
+++ src/handler.php
@@ -6,21 +6,25 @@ define('DEFAULT_FNAME', 'Andrey');
 define('DEFAULT_LNAME', 'Gubin');
 define('DEFAULT_EMAIL', 'girls_like_stars@gmail.com');
 
+$date = new DateTime('now');
+$lastResponseDate = $date->format('Y-m-d H:i:s');
+
 if (empty($_POST['first_name']) || empty($_POST['last_name']) || empty($_POST['email'])) {
     $first_name = DEFAULT_FNAME;
     $last_name = DEFAULT_LNAME;
     $email = DEFAULT_EMAIL;
+    $resends_number = 1;
+    $last_send = $lastResponseDate;
 } else {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $email = $_POST['email'];
+    $resends_number = 1;
+    $last_send = $lastResponseDate;
 } 
-
-$date = new DateTime('now');
-$lastResponseDate = $date->modify('+2 hours')->format('Y-m-d H:i:s');
 
 $csvPath = getenv('CSV_PATH');
 $user = new User($csvPath);
-$message = $user->addUser($first_name, $last_name, $email);
+$message = $user->addUser($first_name, $last_name, $email, $resends_number, $last_send);
 
 header("Location: result.php?email=$email&message=$message");
