fb55c23bcf711ad9349e858beb741ce52106be64
oromadin <osromadin@gmail.com>
2025-04-16 20:31:58 +0300

    filter data. prepare data for import

--- src/Requests.php
+++ src/Requests.php
@@ -66,8 +66,10 @@ class Requests
     {
         require_once $this->csvPath;
 
-        $sortedData = [];
-        $importLog = [
+        $emails = [];
+        $noErrorsData = [];
+        $result = [];
+        $filterLog = [
             "success" => 0,
             "duplicate" => 0,
             "errors" => 0,
@@ -75,24 +77,41 @@ class Requests
         ];
 
         if (empty(DATA)) {
-            echo 'No data for import';
+            return null;
         }
 
         foreach (DATA as $item) {
+            $filterLog['total'] += 1; 
+            
             if (empty($item['first_name']) || empty($item['last_name']) || empty($item['email'])) {
-                $importLog['errors'] += 1;
+                $filterLog['errors'] += 1;
             } else {
-                $sortedData[] = $item;
+                $noErrorsData[] = $item;
+                $emails[] = $item['email'];
             }
         }
 
+        $withDublicates = count($emails);
+        $emails = array_unique($emails, SORT_STRING);
+        $withOutDublicates = count($emails);
+        
+        $filterLog['duplicate'] = $withDublicates - $withOutDublicates;
+        $filterLog['success'] = $withOutDublicates;
+        
+        foreach ($emails as $key => $email) {
+            if (array_key_exists($key, $noErrorsData)) {
+                $result[] = $noErrorsData[$key];
+            }
+        }   
+
         echo '<pre>';
-        print_r($sortedData);
+        print_r($filterLog);
         echo '</pre>';
 
         echo '<pre>';
-        print_r($importLog);
+        print_r($result);
         echo '</pre>';
 
+        return $filterLog;
     }
 }
